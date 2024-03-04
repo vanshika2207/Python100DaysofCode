a1a1d98a523e0b467ee3d76edd844130c39f2b13
vanshika2207 <100851563+vanshika2207@users.noreply.github.com>
2024-03-04 11:17:23 +0530

    Create day1.py

---
+++ day1.py
@@ -0,0 +1,33 @@
+# '''
+# #print function
+# print("hello world")
+# print()
+# print("hello\nhello")
+# print('')
+# print('a'+'b')
+# print('a'+' b')
+# print('a '+'b')
+# print('a'+' '+'b')
+# '''
+# """
+# #input function
+# print("hello " + input("what is your name?"))
+# """
+
+# #variable
+# # name=input('what is your name ?')
+# # print(name)
+
+# name="jack"
+# print(name)
+# name="jacky"
+# print(name)
+
+#band name generator
+print("Welcome to the Band Name Generator.")
+
+city = input("What's name of the city you grew up in?\n")
+
+pet = input("What's your pet's name?\n")
+
+print("Your band name could be " + city + " " + pet)
