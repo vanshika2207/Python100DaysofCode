7b5e4b23366e26a350dc8f07e5f6b815909cdb69
vanshika2207 <100851563+vanshika2207@users.noreply.github.com>
2024-03-23 11:52:52 +0530

    Update day9.py

--- day9.py
+++ day9.py
@@ -1,1 +1,26 @@
 
+#the secret auction program
+
+bid={}
+name=input("What is your name")
+price=int(input("What is the bid price"))
+bid[name]=price
+
+ans=input("If there are more users who want to bid 'Y' or 'N'").lower()
+while ans=='y':
+  
+  name=input("What is your name")
+  price=int(input("What is the bid price"))
+  bid[name]=price
+  
+  ans=input("If there are more users who want to bid 'Y' or 'N'").lower()
+
+max_bid=0  
+for key in bid:
+  if bid[key]>max_bid:
+    max_bid=bid[key]
+    winner=key
+print("the winner is", winner , "with the bid of :",max_bid) 
+
+  
+          
